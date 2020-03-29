92e6fa226082d9897395583aee65f6c0a19e7804
Ilya Matv <arkashabi4@gmail.com>
2020-03-29 17:10:11 +0300

    add works

--- src/components/Projects/WorksItem/WorksItem.jsx
+++ src/components/Projects/WorksItem/WorksItem.jsx
@@ -4,11 +4,15 @@ import classes from './WorksItem.module.css'
 const WorksItem = (props) => {
 
     return (
-        <div className={classes.worksItem}>
-            <button>Смотреть</button>
-            <div>
-                <p>Название проекта</p>
-                <span>Краткое описание</span>
+        <div className={classes.works__item}>
+            <div className={classes.works__img}>
+                <button className={classes.works__btn}>Смотреть</button>
+            </div>
+            <div className={classes.works__footer}>
+                <div className={classes.works__prj}>
+                    <p>Название проекта</p>
+                    <span>Краткое описание</span>
+                </div>
             </div>
         </div>
     )

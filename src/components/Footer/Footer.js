22bd3eee4999056fec6593873cf0924fbe8a2a39
Ilya Matv <arkashabi4@gmail.com>
2020-03-31 01:37:58 +0300

    add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,14 @@ const Footer = (props) => {
     return (
         <div className={classes.footer}>
             <div className={classes.container}>
-                
+                <div className={classes.footer__name}>Илья Матвейчук</div>
+                <div className={classes.footer__items}>
+                    <div className={classes.footer__item}></div>
+                    <div className={classes.footer__item}></div>
+                    <div className={classes.footer__item}></div>
+                    <div className={classes.footer__item}></div>
+                </div>
+                <div className={classes.copyright}>© 2020 Все права защищены</div>
             </div>
         </div>
     )
